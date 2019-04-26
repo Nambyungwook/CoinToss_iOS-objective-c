85319ef1aebb889622016c4a967b5048fc87e73f
NamByungwook <skaquddnr21@gmail.com>
2019-04-26 14:25:00 +0900

    first commit

--- CoinToss/ViewController.h
+++ CoinToss/ViewController.h
@@ -2,14 +2,21 @@
 //  ViewController.h
 //  CoinToss
 //
-//  Created by 박미경 on 22/04/2019.
+//  Created by 박미경 on 18/04/2019.
 //  Copyright © 2019 Nambyungwook. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    UILabel *status;
+    UILabel *result;
+}
 
+@property (nonatomic, retain) IBOutlet UILabel *status;
+@property (nonatomic, retain) IBOutlet UILabel *result;
+
+-(IBAction)callHeads;
+-(IBAction)callTails;
 
 @end
-
--- CoinToss/ViewController.m
+++ CoinToss/ViewController.m
@@ -2,22 +2,52 @@
 //  ViewController.m
 //  CoinToss
 //
-//  Created by 박미경 on 22/04/2019.
+//  Created by 박미경 on 18/04/2019.
 //  Copyright © 2019 Nambyungwook. All rights reserved.
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import <QuartzCore/QuartzCore.h>
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+@synthesize status, result;
+
+-(void) simulateCoinToss:(BOOL)userCallHeads {
+    BOOL coinLandedOnHeads = (arc4random() % 2) == 0;
+    result.text = coinLandedOnHeads ? @"Heads" : @"Tails";
+    
+    if (coinLandedOnHeads == userCallHeads)
+        status.text = @"Correct!";
+    else
+        status.text = @"Wrong!";
+    
+    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    rotation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    rotation.fromValue = [NSNumber numberWithFloat:0.0f];
+    rotation.toValue = [NSNumber numberWithFloat:720 * M_PI / 180.0f];
+    rotation.duration = 2.0f;
+    [status.layer addAnimation:rotation forKey:@"ratate"];
+    
+    CABasicAnimation *fade = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    fade.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    fade.fromValue = [NSNumber numberWithFloat:0.0f];
+    fade.toValue = [NSNumber numberWithFloat:1.0f];
+    fade.duration = 3.5f;
+    [status.layer addAnimation:fade forKey:@"fade"];
 }
 
+-(IBAction) callHeads {
+    [self simulateCoinToss:YES];
+}
+
+-(IBAction) callTails {
+    [self simulateCoinToss:NO];
+}
+
+-(void) viewDidUnload {
+    self.status = nil;
+    self.result = nil;
+}
 
 @end
